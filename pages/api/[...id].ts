1c62daf24f09d4f059e6bb9f0d4afb96a8f266f5
BASSEM <bassem.jadoui@esprit.tn>
2023-08-14 16:26:56 +0100

    install, configure and apply prettier

--- pages/api/[...id].ts
+++ pages/api/[...id].ts
@@ -1,53 +1,53 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 import {
-  zero,
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight,
-  nine,
-} from "../../libs/numbers";
-import { initializeApp } from "firebase/app";
-import { child, get, getDatabase, ref, set } from "firebase/database";
-import getImg from "../../libs/getImg";
-import fillGaps from "../../libs/fillGaps";
+    zero,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+} from '../../libs/numbers'
+import { initializeApp } from 'firebase/app'
+import { child, get, getDatabase, ref, set } from 'firebase/database'
+import getImg from '../../libs/getImg'
+import fillGaps from '../../libs/fillGaps'
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse,
 ) {
-  const { id } = req.query;
-  console.log('query: ', req.query);
+    const { id } = req.query
+    console.log('query: ', req.query)
 
-  const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "visitor-count-66755.firebaseapp.com",
-    projectId: "visitor-count-66755",
-    storageBucket: "visitor-count-66755.appspot.com",
-    messagingSenderId: "618673330830",
-    appId: "1:618673330830:web:73f8178c7fa23cb87407df",
-    databaseURL:
-      "https://visitor-count-66755-default-rtdb.europe-west1.firebasedatabase.app",
-  };
+    const firebaseConfig = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: 'visitor-count-66755.firebaseapp.com',
+        projectId: 'visitor-count-66755',
+        storageBucket: 'visitor-count-66755.appspot.com',
+        messagingSenderId: '618673330830',
+        appId: '1:618673330830:web:73f8178c7fa23cb87407df',
+        databaseURL:
+            'https://visitor-count-66755-default-rtdb.europe-west1.firebasedatabase.app',
+    }
 
-  await initializeApp(firebaseConfig);
+    await initializeApp(firebaseConfig)
 
-  const dbRef = await ref(getDatabase());
-  get(child(dbRef, `users/`))
-    .then(async snapshot => {
-      if (snapshot.exists()) {
-        const visits = (snapshot.val()[`${id}`] || 0) + 1;
+    const dbRef = await ref(getDatabase())
+    get(child(dbRef, `users/`))
+        .then(async snapshot => {
+            if (snapshot.exists()) {
+                const visits = (snapshot.val()[`${id}`] || 0) + 1
 
-        const db = await getDatabase();
-        await set(ref(db, "users/" + id), visits);
+                const db = await getDatabase()
+                await set(ref(db, 'users/' + id), visits)
 
-        res.setHeader("Content-Type", "image/svg+xml; charset=utf-8");
-        res.send(
-          `<svg
+                res.setHeader('Content-Type', 'image/svg+xml; charset=utf-8')
+                res.send(
+                    `<svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
         width="260"
@@ -57,37 +57,37 @@ export default async function handler(
       <title>Visitor Count</title>
       <g style='transform: translateY(-25px)'>
         ${fillGaps(visits)
-          .map((d: string, i: number) => {
-            const number =
-              d === "1"
-                ? one
-                : d === "2"
-                ? two
-                : d === "3"
-                ? three
-                : d === "4"
-                ? four
-                : d === "5"
-                ? five
-                : d === "6"
-                ? six
-                : d === "7"
-                ? seven
-                : d === "8"
-                ? eight
-                : d === "9"
-                ? nine
-                : zero;
+            .map((d: string, i: number) => {
+                const number =
+                    d === '1'
+                        ? one
+                        : d === '2'
+                        ? two
+                        : d === '3'
+                        ? three
+                        : d === '4'
+                        ? four
+                        : d === '5'
+                        ? five
+                        : d === '6'
+                        ? six
+                        : d === '7'
+                        ? seven
+                        : d === '8'
+                        ? eight
+                        : d === '9'
+                        ? nine
+                        : zero
 
-            return getImg(i * 35, number);
-          })
-          .join("")}
+                return getImg(i * 35, number)
+            })
+            .join('')}
       </g>
-    </svg>`
-        );
-      }
-    })
-    .catch(error => {
-      console.error(error);
-    });
+    </svg>`,
+                )
+            }
+        })
+        .catch(error => {
+            console.error(error)
+        })
 }
--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,27 +1,27 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 import {
-  zero,
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight,
-  nine,
-} from "../../libs/numbers";
+    zero,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+} from '../../libs/numbers'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const visits = "0001235";
+    const visits = '0001235'
 
-  const getImg = (x: number, data: string) => {
-    return `<image x="${x}" y="0" width="45" height="100" xlink:href="${data}"/>`;
-  };
+    const getImg = (x: number, data: string) => {
+        return `<image x="${x}" y="0" width="45" height="100" xlink:href="${data}"/>`
+    }
 
-  res.setHeader("Content-Type", "image/svg+xml; charset=utf-8");
-  res.send(
-    `<svg
+    res.setHeader('Content-Type', 'image/svg+xml; charset=utf-8')
+    res.send(
+        `<svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
         width="315"
@@ -31,33 +31,33 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       <title>Visitor Count</title>
       <g>
         ${visits
-          .split("")
-          .map((d, i) => {
-            const number =
-              d === "1"
-                ? one
-                : d === "2"
-                ? two
-                : d === "3"
-                ? three
-                : d === "4"
-                ? four
-                : d === "5"
-                ? five
-                : d === "6"
-                ? six
-                : d === "7"
-                ? seven
-                : d === "8"
-                ? eight
-                : d === "9"
-                ? nine
-                : zero;
+            .split('')
+            .map((d, i) => {
+                const number =
+                    d === '1'
+                        ? one
+                        : d === '2'
+                        ? two
+                        : d === '3'
+                        ? three
+                        : d === '4'
+                        ? four
+                        : d === '5'
+                        ? five
+                        : d === '6'
+                        ? six
+                        : d === '7'
+                        ? seven
+                        : d === '8'
+                        ? eight
+                        : d === '9'
+                        ? nine
+                        : zero
 
-            return getImg(i * 35, number);
-          })
-          .join("")}
+                return getImg(i * 35, number)
+            })
+            .join('')}
       </g>
-    </svg>`
-  );
+    </svg>`,
+    )
 }
