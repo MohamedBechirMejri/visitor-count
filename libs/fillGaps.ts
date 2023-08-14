1c26a6c7408484f3628ee6c278a9607247c065a2
BASSEM <bassem.jadoui@esprit.tn>
2023-08-14 16:29:06 +0100

    final prettier update

--- libs/fillGaps.ts
+++ libs/fillGaps.ts
@@ -1,8 +1,8 @@
 const fillGaps = (number: number): number[] => {
-  let array = number.toString().split("");
-  for (let i = array.length; i < 7; i++) {
-    array.unshift("0");
-  }
-    return array.map((d) => parseInt(d));
-};
-export default fillGaps;
+    let array = number.toString().split('')
+    for (let i = array.length; i < 7; i++) {
+        array.unshift('0')
+    }
+    return array.map(d => parseInt(d))
+}
+export default fillGaps
