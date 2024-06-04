b0d3fb6fd3c100266a58f2e6c3603770d464671a
Mohamed Bechir Mejri <76930306+MohamedBechirMejri@users.noreply.github.com>
2024-06-04 20:39:32 +0000

    chore: Refactor API handler to track unique views and improve code readability

--- app/api/[[...id]]/route.ts
+++ app/api/[[...id]]/route.ts
@@ -1,11 +1,11 @@
 
-import { initializeApp } from 'firebase/app'
-import { child, get, getDatabase, ref, set } from 'firebase/database'
-import { hexToCSSFilter } from 'hex-to-css-filter'
-import fillGaps from '../../../libs/fillGaps'
-import getImg from '../../../libs/getImg'
-import { Numbers } from '../../../libs/numbers'
-
+import { initializeApp } from 'firebase/app';
+import { child, get, getDatabase, ref, set } from 'firebase/database';
+import { hexToCSSFilter } from 'hex-to-css-filter';
+import fillGaps from '../../../libs/fillGaps';
+import getImg from '../../../libs/getImg';
+import { Numbers } from '../../../libs/numbers';
+import { NextRequest } from 'next/server';
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -16,91 +16,89 @@ const firebaseConfig = {
     appId: '1:618673330830:web:73f8178c7fa23cb87407df',
     databaseURL:
         'https://visitor-count-66755-default-rtdb.europe-west1.firebasedatabase.app',
-}
+};
 
-export async function GET(req: Request, { params }: { params: { id: string[], hexColor: string } }) {
-    let { id: ID, hexColor } = params
+export async function GET(req: NextRequest, { params }: { params: { id: string[]; } }) {
+    let { id: ID } = params;
+    const id = ID.join(',');
 
-    const id = ID.join(',')
+    const url = new URL(req.url);
+    const hexColor = url.searchParams.get('hexColor');
 
-    console.log('ID', id)
-    console.log('HEX COLOR', hexColor)
+    const isLoser = ID && ID[0] === 'AminDhouib';
 
-    // const XFF = req.headers['X-Forwarded-For']
+    const ip = req.ip || req.headers.get('X-Forwarded-For') || '0.0.0.0'
 
-    const isLoser = id && id[0] === 'AminDhouib'
+    console.log('ip', ip);
 
-    const ip = 0
-
-
-    if (isLoser) return new Response(`<svg height="40" width="200" xmlns="http://www.w3.org/2000/svg">
-    <text x="5" y="30" fill="none" stroke="red" font-size="35">I'm Gay 🥖</text>
-    </svg>`, {
-        status: 200,
-        headers: { "Content-Type": 'image/svg+xml; charset=utf-8' },
-    });
+    if (isLoser) {
+        return new Response(
+            `<svg height="40" width="200" xmlns="http://www.w3.org/2000/svg">
+                <text x="5" y="30" fill="none" stroke="red" font-size="35">I'm Gay 🥖</text>
+            </svg>`,
+            {
+                status: 200,
+                headers: { "Content-Type": 'image/svg+xml; charset=utf-8' },
+            }
+        );
+    }
 
     const filter = hexColor
         ? hexToCSSFilter(`#${hexColor}`).filter
-        : 'invert(68%) sepia(17%) saturate(1307%) hue-rotate(106deg) brightness(94%) contrast(91%)'
+        : 'invert(68%) sepia(17%) saturate(1307%) hue-rotate(106deg) brightness(94%) contrast(91%)';
 
-    await initializeApp(firebaseConfig)
+    try {
+        await initializeApp(firebaseConfig);
 
-    const dbRef = await ref(getDatabase())
+        const dbRef = ref(getDatabase());
+        const snapshot = await get(child(dbRef, `users/`));
 
-    get(child(dbRef, `users/`))
-        .then(async snapshot => {
-            if (snapshot.exists()) {
-                const result = snapshot.val()
+        if (snapshot.exists()) {
+            const result = snapshot.val();
+            let user = result[id] || 0;
 
-                let user = result[`${id}`] || 0
+            // check if user is of type object
+            if (typeof user === 'number') {
+                user = { views: user, uniqueViews: [] };
+            }
 
-                // check if user is of type object
-                if (typeof user === 'number') {
-                    user = { views: user, uniqueViews: [] }
-                }
+            const views = (user.views || 0) + 1;
+            const uniqueViews = (user.uniqueViews || []).includes(ip) ? user.uniqueViews || [] : [...user.uniqueViews, ip];
 
-                const views = (user.views || 0) + 1
-                const uniqueViews = (user.uniqueViews || []).includes(ip) ? user.uniqueViews || [] : [...user.uniqueViews, ip]
+            const db = getDatabase();
+            await set(ref(db, 'users/' + id), { views, uniqueViews });
 
-                const db = await getDatabase()
-                await set(ref(db, 'users/' + id), { views, uniqueViews })
-
-                return new Response(
-                    `<svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlns:xlink="http://www.w3.org/1999/xlink"
-                        width="260"
-                        height="69"
-                        version="1.1"
-                        style='filter: ${filter}; font-family: monospace, sans-serif; font-size: 10px;'>
-
-                        <title>Visitor Count</title>
-                        <g style='transform: translate(0, -26px); scale: 1;'>
-                            ${fillGaps(uniqueViews.length)
-                        .map((d: number, i: number) =>
-                            getImg(i * 35, Numbers[d]),
-                        )
-                        .join('')}
-                        </g>
-
-                        <text x="0" y="60" fill="none" stroke="black" font-size="14" style='transform: translate(24%, 8%);'>
-                            total views: ${views}
-                        </text>
-                    </svg>`, {
+            return new Response(
+                `<svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    width="260"
+                    height="69"
+                    version="1.1"
+                    style='filter: ${filter}; font-family: monospace, sans-serif; font-size: 10px;'>
+                    <title>Visitor Count</title>
+                    <g style='transform: translate(0, -26px); scale: 1;'>
+                        ${fillGaps(uniqueViews.length)
+                    .map((d, i) => getImg(i * 35, Numbers[d]))
+                    .join('')}
+                    </g>
+                    <text x="0" y="60" fill="none" stroke="black" font-size="14" style='transform: translate(24%, 8%);'>
+                        total views: ${views}
+                    </text>
+                </svg>`,
+                {
                     status: 200,
                     headers: { "Content-Type": 'image/svg+xml; charset=utf-8' },
                 }
-                )
-            }
-        })
-        .catch(error => {
-            console.error(error)
-            return new Response('Error' + error, { status: 500 })
-        })
+            );
+        } else {
+            return new Response('No data found', { status: 404 });
+        }
+    } catch (error: any) {
+        console.error(error);
+        return new Response('Error: ' + error.message, { status: 500 });
+    }
 }
 
-
-
 // no cache
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
